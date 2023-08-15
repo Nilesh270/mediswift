6cf844a0ae66a57ecacad5814beec8e7629df2a0
FlaSH514 <amitbabaninayak@gmail.com>
2023-08-15 11:49:08 +0530

    blogs footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Bottom from "./components/Bottom";
+import Blogs from "./components/Blogs";
 const App = () => {
   return (
-    <Navbar/>
-  )
-}
+    <>
+      <Navbar />
+      <Blogs />
+      <Bottom />
+    </>
+  );
+};
 
 export default App;
