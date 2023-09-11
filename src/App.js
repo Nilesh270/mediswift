0e42c1066c65e6387ac8b44f2f01a8f745a87416
FlaSH514 <amitbabaninayak@gmail.com>
2023-09-11 22:49:27 +0530

    Lab Test Blogs 11Sept

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import LabTests from "./pages/LabTests";
 
 const App = () => {
   return (
     <>
-      <Home/>
-      <Blogs/>
-      <Login/>
-      <Register/>
+      <Home />
+      <Blogs />
+      <Login />
+      <Register />
+      <LabTests />
     </>
   );
 };
